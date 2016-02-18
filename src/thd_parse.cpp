projectceladon/thermal_daemon

commit f99f2b59fbbca04a13cad3f7d2dbc985bc7ee0cd

change the default location of auto file
on Android system folder is not writtable by system process so we
need to put our file to the data folder in order to be able
to create the file
Signed-off-by: Radivoje Jovanovic <radivoje.jovanovic@intel.com>

Reviewed-by: Jovanovic, Radivoje <radivoje.jovanovic@intel.com>
Tested-by: Jovanovic, Radivoje <radivoje.jovanovic@intel.com>

src/thd_parse.cpp
@@ -70,9 +70,8 @@ char *cthd_parse::char_trim(char *str) {
 
 cthd_parse::cthd_parse() :
 		matched_thermal_info_index(-1), doc(NULL), root_element(NULL) {
-	std::string name_conf = TDCONFDIR;
 	std::string name_run = TDRUNDIR;
-	filename = name_conf + "/" + "thermal-conf.xml";
+	filename = name_run + "/" + "thermal-conf.xml";
 	filename_auto = name_run + "/" + "thermal-conf.xml.auto";
 }
 

commit 2c4938d1bf33fbd8fac044cb8a4c834c54b53f5d

Regression for default config file
f99f2b59fbbca04a13cad3f7d2dbc985bc7ee0cd caused regression
where for Android the default path is changed to TDRUNDIR.
This will cause issue to upgrade thermald.

Thanks to Bruno Pagani "ArchangeGabriel" to identify and root
causing this.

src/thd_parse.cpp
@@ -60,8 +60,13 @@ char *cthd_parse::char_trim(char *str) {
 
 cthd_parse::cthd_parse() :
 		matched_thermal_info_index(-1), doc(NULL), root_element(NULL) {
+	std::string name_conf = TDCONFDIR;
 	std::string name_run = TDRUNDIR;
+#ifdef ANDROID
 	filename = name_run + "/" + "thermal-conf.xml";
+#else
+	filename = name_conf + "/" + "thermal-conf.xml";
+#endif
 	filename_auto = name_run + "/" + "thermal-conf.xml.auto";
 }
 